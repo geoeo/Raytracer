8759af7823ca73c7d0c21ee5d8a48921e8c7a9ae
Marc Haubenstock <marchaubenstock@Marcs-MacBook-Pro.local>
2018-03-18 20:27:27 +0100

    implemented simple framebuffer

--- raytracer/Program.fs
+++ raytracer/Program.fs
@@ -1,23 +1,34 @@
 ﻿// Learn more about F# at http://fsharp.org
 
-open System
 open SixLabors.ImageSharp
 open System.IO
 
-let createImage = 
+let width = 640
+let height = 480
+
+let raySampes = [0.1f..0.1f..100.0f]
+let frameBuffer = Array2D.create width height Rgba32.DarkGray
+let squareRange = [200..300]
+
+let render = 
     using (File.OpenWrite("test.jpg")) (fun output ->
-        using(new Image<Rgba32>(640, 480))(fun image -> 
-            for px in 200..300 do
-                for py in 200..300 do
-                    image.Item(px,py) <- Rgba32.White
+        using(new Image<Rgba32>(width,height))(fun image -> 
+            for px in 0..width-1 do
+                for py in 0..height-1 do
+                    if List.exists ( px.Equals ) squareRange &&  List.exists (py.Equals) squareRange then
+                        image.Item(px,py) <- Rgba32.White        
+                    else
+                        image.Item(px,py) <- frameBuffer.[px,py]
+                    
             image.SaveAsJpeg(output)
         )
     )  
 
+
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    createImage
+    render
     0 // return an integer exit code
 
 
