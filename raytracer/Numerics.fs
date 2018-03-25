5e880b417fdf723f88b57f4c5c4f7a5095a9d7ca
Marc Haubenstock <marchaubenstock@Marcs-MacBook-Pro.local>
2018-03-25 13:43:00 +0200

    refactored pixel conversions

--- raytracer/Numerics.fs
+++ raytracer/Numerics.fs
@@ -3,7 +3,6 @@ module Numerics
 open System.Numerics
 open System
 
-
 let normalized(value : Vector3) = 
     if value.Length() = 1.0f then value else failwith "Vector3 not normalized"
 
@@ -11,19 +10,6 @@ let unitParameter (value : float32) =
     if value >= 0.0f && value <= 1.0f then value else failwith "parameter not in range [0,1]"
 
 let round (num :float32) (digits:int) = MathF.Round(num,digits)
-
-// ndc is [0,1] from top left
-let pixelNDC x y width height =
-    (x + 0.5f)/width , (y+0.5f)/height
-
-// screen space is [-1,1] from top left
-let pixelScreen x_ndc y_ndc = 2.0f*x_ndc-1.0f , 1.0f-2.0f*y_ndc
-
-let aspectRatio (width:float32) (height:float32) = width/height
-
-// pixel coordiantes / sample points (X,Y) in camera space (3D)
-let pixelCamera x_screen y_screen aspectRatio fov =
-    x_screen*aspectRatio*MathF.Tan(fov/2.0f) , y_screen*MathF.Tan(fov/2.0f)
 
 // ray direction wrt to camera
 let rayDirection cameraPixel_x cameraPixel_y =
